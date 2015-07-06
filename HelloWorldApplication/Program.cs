5082149c4e1f0a814e58205c68e4fdbb3b07c413
matthewmorris41 <matthewmorris41@gmail.com>
2015-07-06 17:05:39 -0400

    initial commit

---
+++ HelloWorldApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String firstName = "Matthew";
+            String lastName = "Morris";
+            //Write to console Hello, Matthew, Morris
+            Console.WriteLine("Hello, " + firstName + ", " + lastName);
+            //Write to console Hello, Morris, Matthew
+            Console.WriteLine(" " + lastName + ", " + firstName);
+            Console.WriteLine("What is your first name?");
+            
+            firstName =Console.ReadLine();
+            Console.WriteLine(firstName);
+        }
+    }
+}
